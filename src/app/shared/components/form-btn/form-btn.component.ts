6b07f4c813db3f0225a94637c6a56503a662897f
IsaiaRiva <isaia.riva@finconsgroup.com>
2022-10-24 19:59:55 +0200

    and day

--- src/app/shared/components/form-btn/form-btn.component.ts
+++ src/app/shared/components/form-btn/form-btn.component.ts
@@ -3,8 +3,23 @@ import { formBtnDefault } from './form-btn-model';
 
 @Component({
   selector: 'app-form-btn',
-  template: ` <button [disabled]="disable">{{ setUp.label }}</button> `,
-  styles: [],
+  template: `
+    <button class="form-btn" [disabled]="disable">
+      {{ setUp.label }}
+    </button>
+  `,
+  styles: [
+    `
+      .form-btn {
+        width: 100%;
+        border-radius: var(--b-radius-alpha);
+        padding: var(--p-alpha);
+        box-shadow: var(--box-shadow-alpha);
+        font-size: var(--fs-alpha);
+        background: var(--c-Florida);
+      }
+    `,
+  ],
 })
 export class FormBtnComponent implements OnInit {
   @Input() setUp = formBtnDefault;
